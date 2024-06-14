dfbe46ba1627fd5170c8b2d69c8294302c3df427
Rajat Mishra <77861069+mst-rajatmishra@users.noreply.github.com>
2024-06-14 17:22:54 +0530

    Update TODOlistGUI.py

--- TODOlistGUI.py
+++ TODOlistGUI.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-
+ 
 def addtask():
     task=entry.get()
     if task:
@@ -8,7 +8,7 @@ def addtask():
         entry.delete(0,END)
     else:
         messagebox.showwarning("Warning","Please enter a task")
-
+ 
 def removetask():
     selectedtaskindex=listbox.curselection()
     if selectedtaskindex:
@@ -16,28 +16,36 @@ def removetask():
         messagebox.showwarning('Warning',"Task deleted")
     else:
         messagebox.showwarning("Warning","Please select a task to remove")
-
+ 
 def clearlist():
     listbox.delete(0,END)
     messagebox.showwarning('Warning',"List cleared")
-
+ 
 root=Tk()
 root.title("To-Do List")
+root.geometry("600x600")
+root.resizable(False,False)
+root.configure(background="orange")
 
-entry=Entry(root, ipadx=5)
+entry=Entry(root, width=25)
 
+entry.pack(pady=10)
+ 
 add_button=Button(root, text="Add Task", command=addtask)
-
+add_button.pack(pady=10)
 remove_button=Button(root, text="Delete Task", command=removetask)
-
+remove_button.pack(pady=10)
+ 
 clear_button=Button(root, text='Clear List', command=clearlist)
-listbox=Listbox(root)
-
-entry.pack()
-add_button.pack()
-
-remove_button.pack()
-clear_button.pack()
-listbox.pack()
-
+clear_button.pack(pady=10)
+listbox=Listbox(root, width = 40, height= 50 )
+listbox.pack(pady=10)
+ 
+# entry.pack()
+# add_button.pack()
+ 
+# remove_button.pack()
+# clear_button.pack()
+# listbox.pack()
+ 
 root.mainloop()
